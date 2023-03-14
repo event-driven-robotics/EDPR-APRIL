79bcae624add8ac26c642aaf9ee9c87e256318d8
Franco Di Pietro <francodipietro89@gmail.com>
2023-03-14 13:30:29 +0000

    Mod: ROS_MASTER_URI as parameter in bash script

--- run_april.sh
+++ run_april.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
-export ROS_MASTER_URI=http://127.0.0.1:11311
+
+while getopts 'dm:' OPTION; do
+    case "$OPTION" in
+    d)
+        echo "ROS_MASTER_URI set to default value"
+        export ROS_MASTER_URI=http://127.0.0.1:11311
+        echo "ROS_MASTER_URI="$ROS_MASTER_URI
+        ;;
+    m)
+        echo "ROS_MASTER_URI set manually"
+        export ROS_MASTER_URI="$OPTARG"
+        echo "ROS_MASTER_URI="$ROS_MASTER_URI
+        ;;
+    ?)
+        echo "script usage: /run_april.sh [-d (default)] or [-m value (manual)]" >&2
+        exit 1
+        ;;
+    esac
+done
+
+if [ "$#" -eq  "0" ]
+   then
+    echo "ROS_MASTER_URI not set"
+    exit 1
+fi
+
 echo "Run YARP server connected to ROS"
 yarpserver --ros &
 echo "Run ATIS-bridge"
