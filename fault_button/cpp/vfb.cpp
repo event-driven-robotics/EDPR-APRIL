1e7ff81d60aa566afe7589e55fa3faf2dddbb566
arrenglover <arren.glover@iit.it>
2023-04-21 09:21:18 +0000

    draw fault button region

--- fault_button/cpp/vfb.cpp
+++ fault_button/cpp/vfb.cpp
@@ -1,5 +1,5 @@
 /* 
-Author: Franco Di Pietro, Arren Glover
+Author: Arren Glover
  */
 
 #include <yarp/os/all.h>
@@ -26,12 +26,22 @@ private:
     double p{0.01};      // detection rate
     double T{2e6};       // threshold events/second
 
-    enum state_name{DISPLAY, FBR_DRAW, MONITOR};
+    enum state_name{DISPLAY, FBR_DRAW, MONITOR, FINISHED};
     state_name state{DISPLAY};
+    cv::Mat img, mask;
+
+    typedef struct circle_parameters {
+        cv::Point c;
+        int       r;
+        bool      d;
+    } circle_parameters;
+
+    circle_parameters c_fbr{{0,0},0,false};
 
 public:
     bool configure(yarp::os::ResourceFinder &rf) override
     {
+
         // =====SET UP YARP=====
         if (!yarp::os::Network::checkNetwork(2.0))
         {
@@ -55,12 +65,14 @@ public:
         T = rf.check("T", Value(2e6)).asFloat64();
 
         // ===== TRY DEFAULT CONNECTIONS =====
-        Network::connect("/file/ch0dvs:o", getName("/AE:i"), "fast_tcp");
-        Network::connect("/atis3/AE:o", getName("/AE:i"), "fast_tcp");
+        Network::connect("/file/ch0dvs:o",  getName("/AE:i"), "fast_tcp");
+        Network::connect("/file/atis/AE:o", getName("/AE:i"), "fast_tcp");
+        Network::connect("/atis3/AE:o",     getName("/AE:i"), "fast_tcp");
 
+        img = cv::Mat(img_size, CV_8UC3); img = 0;
+        mask = cv::Mat(img_size, CV_8U); mask = 0;
         cv::namedWindow(getName(),  cv::WINDOW_KEEPRATIO);
         cv::resizeWindow(getName(), {640, 480});
-
 
 
         return true;
@@ -84,24 +96,95 @@ public:
         return true;
     }
 
-    //
+    static void mouseCall(int event, int x, int y, int flags, void* param)
+    {
+        circle_parameters *fbr = reinterpret_cast<circle_parameters*>(param);
+
+        if(event == cv::EVENT_LBUTTONDOWN) {
+            fbr->r = 0;
+            fbr->c = {x, y};
+            fbr->d = true;
+        } else if(event == cv::EVENT_MOUSEMOVE && fbr->d) {
+            fbr->r = sqrt((fbr->c.x-x)*(fbr->c.x-x) + (fbr->c.y-y)*(fbr->c.y-y));
+        } else if(event == cv::EVENT_LBUTTONUP) {
+            fbr->d = false;
+        }
+    }
+
+    void makeMask()
+    {
+        for(int x = 0; x < img_size.width; x++) {
+            for(int y = 0; y < img_size.height; y++) {
+                int dist = sqrt((x-c_fbr.c.x)*(x-c_fbr.c.x)+(y-c_fbr.c.y)*(y-c_fbr.c.y));
+                if(dist < c_fbr.r)
+                    mask.at<char>(y, x) = 1;
+            }
+        }
+    }
+    
     void display()
     {
-        ev::info stat = input_events.readSlidingWinT(1.0);
+        ev::info stat = input_events.readSlidingWinT(0.5);
 
-        static cv::Mat img(img_size, CV_8U);
+        img = 0;
+        for(auto &v : input_events)
+            img.at<cv::Vec3b>(v.y, v.x) = {128, 128, 128};
         
-        
+        cv::Mat img_display; img.copyTo(img_display);
+        cv::putText(img_display, "Press SPACE when environment is clear", cv::Point(img_size.width*0.05, img_size.height*0.95), cv::FONT_HERSHEY_PLAIN, 1.0, {255, 255, 255});
+
+        cv::imshow(getName(), img_display);
+        char c = cv::waitKey(1);
+
+        if(c == '\e')
+            state = FINISHED;
+        else if(c == ' ') {
+            cv::setMouseCallback(getName(), mouseCall, &c_fbr);
+            state = FBR_DRAW;
+        }
     }
 
     void fbr_draw()
     {
+        cv::Mat img_display; img.copyTo(img_display);
+        cv::putText(img_display, "DRAW Fault Region. Click and drag.", cv::Point(img_size.width*0.05, img_size.height*0.95), cv::FONT_HERSHEY_PLAIN, 1.0, {255, 255, 255});
+        
+        cv::circle(img_display, c_fbr.c, c_fbr.r, {120, 10, 10}, -1);
+        cv::circle(img_display, c_fbr.c, c_fbr.r, {255, 0, 0}, 4);
 
+
+        cv::imshow(getName(), img_display);
+        char c = cv::waitKey(1);
+        if(c == '\e')
+            state = FINISHED;
+        else if(c == ' ') {
+            cv::setMouseCallback(getName(), nullptr);
+            makeMask();
+            state = MONITOR;
+        }
     }
 
     void monitor()
     {
 
+        ev::info stat = input_events.readSlidingWinT(k);
+
+        img = 0;
+        cv::circle(img, c_fbr.c, c_fbr.r, {50, 50, 50},   -1);
+        cv::circle(img, c_fbr.c, c_fbr.r, {120, 120, 120}, 4);
+        for(auto &v : input_events) {
+            if(mask.at<char>(v.y, v.x))
+                img.at<cv::Vec3b>(v.y, v.x) = {0, 0, 200};
+            else
+                img.at<cv::Vec3b>(v.y, v.x) = {128, 128, 128};
+        }
+        cv::putText(img, "Monitoring Visual Fault Button", cv::Point(img_size.width*0.05, img_size.height*0.95), cv::FONT_HERSHEY_PLAIN, 1.0, {255, 255, 255});
+
+        cv::imshow(getName(), img);
+        char c = cv::waitKey(1);
+
+        if(c == '\e')
+            state = FINISHED;
     }
 
     // synchronous thread
@@ -115,6 +198,8 @@ public:
                 fbr_draw(); break;
             case(MONITOR):
                 monitor(); break;
+            case(FINISHED):
+                return false;
         }
 
 
@@ -129,6 +214,8 @@ int main(int argc, char *argv[])
     yarp::os::ResourceFinder rf;
     rf.setVerbose(false);
     rf.configure(argc, argv);
+
+    yInfo() << "I started";
 
     /* create the module */
     vbfApp instance;
