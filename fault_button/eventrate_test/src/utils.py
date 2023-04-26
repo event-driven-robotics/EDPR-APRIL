e9d52d9e1846e4322d94da8e2bcd61bddfa0e2f4
Stefano Chiavazza <stefano.chiavazza@iit.it>
2023-04-26 16:38:58 +0200

    implemented circle roi

--- fault_button/eventrate_test/src/utils.py
+++ fault_button/eventrate_test/src/utils.py
@@ -2,10 +2,11 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+import cv2
 
 from bimvee.importIitYarp import importIitYarp
 
-def get_roi(center, edge, scale=1.0):
+def get_roi_square(center, edge, scale=1.0):
 
     r = max(
         np.abs(center[0] - edge[0]),
@@ -20,31 +21,44 @@ def get_roi(center, edge, scale=1.0):
         int(center[1] - r),
         int(center[1] + r)
     ]
-    # might return limits outside the image
-    return np.array([xlim, ylim])
+    mask = np.zeros((480, 640))
+    square_mask = cv2.rectangle(mask, (xlim[0], ylim[0]), (xlim[1], ylim[1]), color=(255, 255, 255), thickness=-1)
+
+    window_size = (xlim[1] - xlim[0]) * (ylim[1] - ylim[0])
+
+    return square_mask, window_size
+
+def get_roi_circle(center, edge, scale=1.0):
+    r = int(max(
+        np.abs(center[0] - edge[0]),
+        np.abs(center[1] - edge[1])
+    ) * scale)
+
+    mask = np.zeros((480, 640))
+    circle_mask = cv2.circle(mask, center, r, color=(255, 255, 255), thickness=-1)
+
+    window_size = np.pi * np.square(r)
+
+    return circle_mask.astype(bool), window_size
 
 def get_events_roi(events, roi):
     # select only the events inside the window considered
-    # the size can be changed with d
 
-    events_x = events['data']['left']['dvs']['x']
-    events_y = events['data']['left']['dvs']['y']
+    events_x = np.array(events['data']['left']['dvs']['x'])
+    events_y = np.array(events['data']['left']['dvs']['y'])
 
-    x_lim = roi[0]
-    y_lim = roi[1]
+    # event_mask = np.zeros(events_x.shape, dtype=bool)
 
-    filt_x = np.logical_and(events_x > x_lim[0],
-                    events_x < x_lim[1])
-    filt_y = np.logical_and(events_y > y_lim[0],
-                    events_y < y_lim[1])
+    # for i, (x, y) in enumerate(zip(events_x, events_y)):
+    #     if roi[y, x]:
+    #         event_mask[i] = True
+    # event_mask = [True if roi[y, x] else False for x, y in zip(events_x, events_y)]
 
-    window_size = [x_lim[1] - x_lim[0], y_lim[1] - y_lim[0]]
+    event_mask = roi[events_y, events_x]
 
-    ts_filt = events['data']['left']['dvs']['ts'][np.logical_and(
-        filt_x, filt_y
-    )]
+    ts_filt = events['data']['left']['dvs']['ts'][event_mask]
 
-    return ts_filt, window_size
+    return ts_filt
 
 def get_hist(ts_filt, dataset_tres, t_start=0.0, t_end=np.inf):
    
@@ -93,7 +107,7 @@ def measure_time_difference(detection_times, press_timing):
     return differences
 
 def get_threshold(base_thresh, tres, window_size, scale):
-    return (base_thresh * tres) * (window_size[0] * window_size[1]) * scale
+    return (base_thresh * tres) * window_size
 
 
 class Plotter:
@@ -123,74 +137,17 @@ class Plotter:
         positions = self.positions
         # the roi center is the button position
         roi_center = [
-            positions[0, [2, 4]].sum() / 2,
-            positions[0, [1, 3]].sum() / 2
+            int(positions[0, [2, 4]].sum() / 2),
+            int(positions[0, [1, 3]].sum() / 2)
         ]
         # the roi edge is manually selected close the the person
         roi_edge = [
-            positions[1, [2, 4]].sum() / 2,
-            positions[1, [1, 3]].sum() / 2
+            int(positions[1, [2, 4]].sum() / 2),
+            int(positions[1, [1, 3]].sum() / 2)
         ]
 
         self.roi_center =  roi_center
         self.roi_edge = roi_edge
-
-    def plot_tres(self, base_thresh, dataset_tres_ls, scale, sequence_name, t_start=0, t_end=np.inf):
-
-        events = self.events
-        press_timing = self.press_timing
-        roi_center = self.roi_center
-        roi_edge = self.roi_edge
-
-        fig, axs = plt.subplots(len(dataset_tres_ls), 1, figsize=(15, 15))
-
-        mean_differences = []
-        n_detections = []
-
-        # scale controlls the radius scale
-        roi = get_roi(roi_center, roi_edge, scale=scale)
-        roi[0] = np.clip(roi[0], 0, 640) # hardcoded img resolution
-        roi[1] = np.clip(roi[1], 0, 480)
-
-        ts_filt, window_size = get_events_roi(events, roi)
-
-        for ax, dataset_tres in zip(axs, dataset_tres_ls):
-
-            # plot the histogrm, it is faster to use line plot
-            h, bins = get_hist(ts_filt, dataset_tres, t_start=t_start, t_end=t_end)
-            ax.plot(bins[:-1], h)
-            ax.fill_between(bins[:-1], h)
-            # get y val for markers to be on the top of the plot
-            plt_y_lim = ax.get_ylim()
-            v = plt_y_lim[1]
-
-            # scale the threshold depending on the time resolution and roi dimension
-            detection_thresh = get_threshold(base_thresh, dataset_tres, window_size, scale)
-            print(f"Detection threshold: {detection_thresh}")
-            ax.axhline(y=detection_thresh, color='red', label="Detection threshold")
-
-
-            ax.scatter(press_timing[:, 0], [v for x in press_timing[:, 0]], marker="x", c="green", label="Fault button press", s=80)
-            detection_times = get_detections(h, detection_thresh, bins, dataset_tres)
-            ax.scatter(detection_times, [v for x in detection_times], marker="x", c="blue", label="Detection time", s=80)
-
-
-            ax.ticklabel_format(axis="y", style="sci", scilimits=(0,0))
-            ax.tick_params(labelsize=14)
-            ax.set_title(f"t_res: {dataset_tres}", fontsize=14)
-
-            differences = measure_time_difference(detection_times, press_timing)
-            mean_differences.append(np.mean(differences))
-            n_detections.append(len(detection_times) - len(press_timing))
-
-        fig.suptitle(sequence_name, fontsize=20)
-        fig.supxlabel("Time[s]", fontsize=24)
-        fig.supylabel("Number of events", fontsize=24)
-
-        lines, labels = fig.axes[-1].get_legend_handles_labels()
-        plt.figlegend(lines,labels, ncol=3)
-
-        fig.tight_layout()
         
     def plot_roi_scale(self, base_thresh, dataset_tres, scales, sequence_name):
         
@@ -204,11 +161,8 @@ class Plotter:
         for ax, scale in zip(axs, scales):
 
             # scale controlls the radius scale
-            roi = get_roi(roi_center, roi_edge, scale=scale)
-            roi[0] = np.clip(roi[0], 0, 640) # hardcoded img resolution
-            roi[1] = np.clip(roi[1], 0, 480)
-
-            ts_filt, window_size = get_events_roi(events, roi)
+            roi, window_size = get_roi_circle(roi_center, roi_edge, scale=scale)
+            ts_filt = get_events_roi(events, roi)
 
             h, bins = get_hist(ts_filt, dataset_tres)
             ax.plot(bins[:-1], h)
@@ -250,11 +204,9 @@ class Plotter:
         n_detections = []
 
         # scale controlls the radius scale
-        roi = get_roi(roi_center, roi_edge, scale=scale)
-        roi[0] = np.clip(roi[0], 0, 640) # hardcoded img resolution
-        roi[1] = np.clip(roi[1], 0, 480)
+        roi, window_size = get_roi_circle(roi_center, roi_edge, scale=scale)
 
-        ts_filt, window_size = get_events_roi(events, roi)
+        ts_filt = get_events_roi(events, roi)
 
         for ax, dataset_tres in zip(axs, dataset_tres_ls):
 
@@ -307,11 +259,9 @@ class Plotter:
         for scale in tqdm(scales):
 
             # scale controlls the radius scale
-            roi = get_roi(roi_center, roi_edge, scale=scale)
-            roi[0] = np.clip(roi[0], 0, 640) # hardcoded img resolution
-            roi[1] = np.clip(roi[1], 0, 480)
+            roi, window_size = get_roi_circle(roi_center, roi_edge, scale=scale)
 
-            ts_filt, window_size = get_events_roi(events, roi)
+            ts_filt = get_events_roi(events, roi)
 
             # measure the event rate
             h, bins = get_hist(ts_filt, dataset_tres, t_start=t_start, t_end=t_end)
@@ -357,14 +307,12 @@ class Plotter:
         mean_differences = []
         n_detections = []
 
+        # scale controlls the radius scale
+        roi, window_size = get_roi_circle(roi_center, roi_edge, scale=scale)
+
+        ts_filt = get_events_roi(events, roi)
+
         for dataset_tres in tqdm(dataset_tres_ls):
-
-            # scale controlls the radius scale
-            roi = get_roi(roi_center, roi_edge, scale=scale)
-            roi[0] = np.clip(roi[0], 0, 640) # hardcoded img resolution
-            roi[1] = np.clip(roi[1], 0, 480)
-
-            ts_filt, window_size = get_events_roi(events, roi)
 
             # measure the event rate
             h, bins = get_hist(ts_filt, dataset_tres, t_start=t_start, t_end=t_end)
